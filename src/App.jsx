39d8dbcb8a9f301b394b10f38dd5b8c82b29c9ec
Ngan Dinh Phan Kim <ngandinhphankim@gmail.com>
2023-11-27 23:44:42 +0700

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>3D Portfolio</div>
+  )
+}
+
+export default App
